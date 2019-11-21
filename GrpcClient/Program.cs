9a3f9bd2ab6ed2b0ff5fd49067a4247abfcc918d
松 訾 <Janson1991@outlook.com>
2019-11-22 00:10:39 +0800

    添加项目文件。

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001/");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest()
+            {
+                Name = "JANSON-1991"
+            });
+            Console.WriteLine(reply.Message);
+
+            var client2 = new Jansoner.JansonerClient(channel);
+            var reply2 = await client2.SendInfoAsync(new DataRequest
+            {
+                Id = 1,
+                Name = "janson",
+                Pwd = "123"
+
+            });
+
+            Console.WriteLine(reply2.Message + reply2.IsSuccess);
+
+            using (var reply3 = client2.GetList(new NonMessage()))
+            {
+                while (await reply3.ResponseStream.MoveNext())
+                {
+                    var current = reply3.ResponseStream.Current;
+                    Console.WriteLine("Age" + current.Age + "Email" + current.Email + "Id" + current.Id + "Name" + current.Name);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GrpcService/Services/JanonsServer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcService
+{
+    public class JanonsServer : Jansoner.JansonerBase
+    {
+        public override Task<DataReply> SendInfo(DataRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new DataReply
+            {
+                Message = $"{DateTime.Now} - { request.Name} ",
+                IsSuccess = true
+            });
+        }
+
+        public override async Task GetList(NonMessage request, IServerStreamWriter<ListMessage> responseStream, ServerCallContext context)
+        {
+            var list = new List<ListMessage>();
+
+            list.Add(new ListMessage
+            {
+                Id = "1",
+                Age = "10",
+                Email = "123",
+                Name = "Lucy"
+            });
+
+            list.Add(new ListMessage
+            {
+                Id = "2",
+                Age = "20",
+                Email = "22",
+                Name = "222ucy"
+            });
+            list.Add(new ListMessage
+            {
+                Id = "3",
+                Age = "30",
+                Email = "323",
+                Name = "3Lucy"
+            });
+            list.Add(new ListMessage
+            {
+                Id = "4",
+                Age = "40",
+                Email = "423",
+                Name = "4Lucy"
+            });
+
+            foreach (var msg in list)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(msg);
+            }
+        }
+    }
+}
